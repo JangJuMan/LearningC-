98b97999caf3c0c2204521e753a19301a8a32ecb
JangJuMan <wnaks9901@gmail.com>
2024-02-14 00:27:27 +0900

    2908 solved

--- 2908.cpp
+++ 2908.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+// 숫자 거꾸로 하기
+int reverse(int input){
+    int result = 0;
+
+    while(input != 0){
+        result *= 10;
+        result += input%10;
+        input /= 10;
+    }
+
+    return result;
+}
+
 int main()
 {
-    cout << "hello world!";
+    int input_a, input_b, reverse_a, reverse_b;
+    cin >> input_a >> input_b;
+    reverse_a = reverse(input_a);
+    reverse_b = reverse(input_b);
+    reverse_a > reverse_b ? cout << reverse_a : cout << reverse_b;
+    
+    return 0;
 }
+
